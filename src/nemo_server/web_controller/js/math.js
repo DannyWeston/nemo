21233f2fdf6d9e6de9fddf89df4c7e6429e942aa
Daniel Weston <danielweston37@gmail.com>
2023-03-19 17:38:37 +0000

    Added euler to quaternion support

--- src/nemo_server/web_controller/js/math.js
+++ src/nemo_server/web_controller/js/math.js
@@ -1,3 +1,4 @@
+// Takes a quaternion and returns the 3D euler angles in Radians
 function quaternionToEuler(x, y, z, w){
     const ex = Math.atan2(2 * (x * y + z * w), 1 - (2 * (y * y + z * z)));
       
@@ -8,4 +9,19 @@ function quaternionToEuler(x, y, z, w){
     const euler = [ex, ey, ez];
 
     return(euler);
+}
+
+// Takes normalised euler angles as radians for input and returns a quaternion representation
+function eulerToQuaternion(x, y, z) {
+    const q0 = (Math.cos(x / 2) * Math.cos(y / 2) * Math.cos(z / 2) + Math.sin(x / 2) * Math.sin(y / 2) * Math.sin(z / 2));
+  
+    const q1 = (Math.sin(x / 2) * Math.cos(y / 2) * Math.cos(z / 2) - Math.cos(x / 2) * Math.sin(y / 2) * Math.sin(z / 2));
+  
+    const q2 = (Math.cos(x / 2) * Math.sin(y / 2) * Math.cos(z / 2) + Math.sin(x / 2) * Math.cos(y/ 2) * Math.sin(z / 2));
+  
+    const q3 = (Math.cos(x / 2) * Math.cos(y / 2) * Math.sin(z / 2) - Math.sin(x / 2) * Math.sin(y/ 2) * Math.cos(z / 2));
+  
+    const quat = [q0, q1, q2, q3];
+  
+    return(quat);
 }
